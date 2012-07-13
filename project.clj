9726027d900db084434617b1dc48887a02d8b279
Olivier Bohrer <olivier.bohrer@smallrivers.com>
2012-07-13 10:49:21 +0200

    remove unnecessary deps

--- project.clj
+++ project.clj
@@ -1,10 +1,7 @@
 (defproject clj-jenkins "0.1.0-SNAPSHOT"
   :description "Clojure client for Jenkins"
-  :dependencies [ [org.clojure/clojure            "1.3.0"]
-                  [org.clojars.smallrivers/aleph  "0.2.1-rc4"]
-                  [org.slf4j/slf4j-api            "1.6.1"]
-                  [org.slf4j/slf4j-log4j12        "1.6.1"]
-                  [log4j                          "1.2.16"]]
+  :dependencies [[org.clojure/clojure            "1.3.0"]
+                 [org.clojars.smallrivers/aleph  "0.2.1-rc4"]]
 
   :autodoc {:name       "clj-jenkins"
             :page-title "clojure client for jenkins"})
