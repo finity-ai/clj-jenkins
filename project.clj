fc7afe4cc3462e2f29e520fb8fbbf7810a88fc72
Reynald Borer <reynald.borer@paper.li>
2014-05-23 10:35:45 +0200

    Bump to mainline aleph 0.3.2 (with more recent cheshire)

--- project.clj
+++ project.clj
@@ -1,7 +1,7 @@
-(defproject clj-jenkins "0.1.1"
+(defproject clj-jenkins "0.1.2"
   :description "Clojure client for Jenkins"
-  :dependencies [[org.clojure/clojure            "1.3.0"]
-                 [org.clojars.smallrivers/aleph  "0.2.1-rc4"]]
+  :dependencies [[org.clojure/clojure            "1.5.1"]
+                 [aleph                          "0.3.2" :exclusions [org.clojure/tools.logging]]]
 
   :autodoc {:name       "clj-jenkins"
             :page-title "clojure client for jenkins"})
