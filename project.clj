341417f05f3e5ede5afec030cb13283f4d27b0d2
David Smith <david.smith@paper.li>
2015-07-14 15:47:13 +0200

    added url and license

--- project.clj
+++ project.clj
@@ -1,5 +1,8 @@
 (defproject clj-jenkins "0.1.4"
   :description "Clojure client for Jenkins"
+  :url "https://github.com/smallrivers/clj-jenkins"
+  :license {:name "MIT public License"
+            :url  "http://opensource.org/licenses/MIT"}
   :dependencies [[org.clojure/clojure "1.6.0"]
                  [org.clojure/tools.logging "0.3.1"]
                  [aleph "0.4.0"]
