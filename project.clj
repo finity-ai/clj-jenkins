3549dacb80e274279b33620c1d04489b1c58e7cf
Reynald Borer <reynald.borer@paper.li>
2017-10-09 15:10:58 +0200

    Bump dependencies

--- project.clj
+++ project.clj
@@ -4,9 +4,9 @@
   :license {:name "MIT public License"
             :url  "http://opensource.org/licenses/MIT"}
   :dependencies [[org.clojure/clojure "1.8.0"]
-                 [org.clojure/tools.logging "0.3.1"]
-                 [aleph "0.4.1"]
-                 [cheshire "5.6.3"]]
+                 [org.clojure/tools.logging "0.4.0"]
+                 [aleph "0.4.3"]
+                 [cheshire "5.8.0"]]
   :plugins [[lein-cljfmt "0.5.6"]]
   :autodoc {:name       "clj-jenkins"
             :page-title "clojure client for jenkins"})
