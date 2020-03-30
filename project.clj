dd91ae8a9d4eb5b81f2a2f91260b0ae9a6559b67
Reynald Borer <reynald.borer@paper.li>
2020-03-30 20:24:36 +0200

    Bump dependencies

--- project.clj
+++ project.clj
@@ -1,12 +1,12 @@
 (defproject clj-jenkins "0.1.9-SNAPSHOT"
   :description "Clojure client for Jenkins"
-  :url "https://github.com/smallrivers/clj-jenkins"
+  :url "https://github.com/finity-ai/clj-jenkins"
   :license {:name "MIT public License"
             :url  "http://opensource.org/licenses/MIT"}
-  :dependencies [[org.clojure/clojure "1.8.0"]
-                 [org.clojure/tools.logging "0.4.0"]
-                 [aleph "0.4.3"]
-                 [cheshire "5.8.0"]]
-  :plugins [[lein-cljfmt "0.5.6"]]
+  :dependencies [[org.clojure/clojure "1.10.1"]
+                 [org.clojure/tools.logging "1.0.0"]
+                 [aleph "0.4.6"]
+                 [cheshire "5.10.0"]]
+  :plugins [[lein-cljfmt "0.6.7"]]
   :autodoc {:name       "clj-jenkins"
             :page-title "clojure client for jenkins"})
