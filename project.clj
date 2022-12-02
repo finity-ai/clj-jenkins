2812c686bffec4ebaf63a87f0831afd02fa44b1b
Reynald Borer <361403+rborer@users.noreply.github.com>
2022-12-02 09:46:36 +0100

    Upgrade aleph, cheshire & tools.logging versions

--- project.clj
+++ project.clj
@@ -4,9 +4,9 @@
   :license {:name "MIT public License"
             :url  "http://opensource.org/licenses/MIT"}
   :dependencies [[org.clojure/clojure "1.10.1"]
-                 [org.clojure/tools.logging "1.0.0"]
-                 [aleph "0.4.6"]
-                 [cheshire "5.10.0"]]
-  :plugins [[lein-cljfmt "0.6.7"]]
+                 [org.clojure/tools.logging "1.2.4"]
+                 [aleph "0.5.0"]
+                 [cheshire "5.11.0"]]
+  :plugins [[lein-cljfmt "0.9.0"]]
   :autodoc {:name       "clj-jenkins"
             :page-title "clojure client for jenkins"})
